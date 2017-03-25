8bfd6c8115ac73beab27b8b3ccd176f70ce2382e
rowansharman <rowansharman1@gmail.com>
2017-03-24 23:43:29 -0400

    isbn to basic metadata -works

---
+++ ScannerToData.py
@@ -0,0 +1,39 @@
+import isbnlib
+
+
+def getIsbn():
+    code = input('Scan book barcode or enter ISBN: ')
+    while isbnlib.notisbn(code):
+        code = input('\nPlease scan or enter a valid ISBN: ')
+    return code
+
+
+def printData(data):
+    dataString = ''
+    dataString += ('Title:\t\t' + data['Title'] + '\n')
+
+    plural = 'Author:\t\t'
+    authors = ''
+    for element in data['Authors']:
+        if len(authors) > 1:
+            plural = 'Authors:\t'
+            authors += ', ' + element
+        else:
+            authors += element
+
+    dataString += (plural + authors + '\n')
+    dataString += ('Publisher:\t' + data['Publisher'] + '\n')
+    dataString += ('Year:\t\t' + data['Year'] + '\n')
+    dataString += ('Language:\t' + data['Language'] + '\n')
+
+    print(dataString)
+
+
+if __name__ == '__main__':
+    while True:
+        isbn = getIsbn()
+        info = isbnlib.meta(isbn)
+        if info is None:
+            print("We're sorry, but this book is not in our database.\n")
+        else:
+            printData(info)
