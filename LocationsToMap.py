e2d818b1e1e3cbf32d00de88a8127a6cfba6171f
srbarden <sbarden@wellesley.edu>
2017-03-30 15:56:44 -0400

    adding text box to map -in progress

--- LocationsToMap.py
+++ LocationsToMap.py
@@ -5,6 +5,8 @@ from plotly.graph_objs import Choropleth, Data, Layout, Figure
 
 
 def plotGraph(bookTitle, authName, authLoc, pubName, pubLoc, pubDate, isbn):
+
+    # making a choropleth for the author's location
     author = Choropleth(
         z=['1'],
         autocolorscale=False,
@@ -19,6 +21,7 @@ def plotGraph(bookTitle, authName, authLoc, pubName, pubLoc, pubDate, isbn):
         zmax=1,
         zmin=0,
     )
+    # making a choropleth for the publisher's location.
     publisher = Choropleth(
         z=['1'],
         autocolorscale=False,
@@ -36,8 +39,20 @@ def plotGraph(bookTitle, authName, authLoc, pubName, pubLoc, pubDate, isbn):
 
     data = Data([author, publisher])
 
+    box1 = list(
+        x=0.3,
+        y=0.5,
+        yanchor="top",
+        borderpad=2,
+        bordercolor=rgb(0.5, 0.1, 0.5),     # set this same as color of cluster 1
+        borderwidth=5,
+        text=paste(bookTitle),
+        align="left",
+        showarrow=F)
+
     layout = Layout(
         autosize=True,
+        annotations=list(box1),
         geo=dict(
             countrycolor='rgb(102, 102, 102)',
             countrywidth=0.1,
@@ -62,16 +77,14 @@ def plotGraph(bookTitle, authName, authLoc, pubName, pubLoc, pubDate, isbn):
         showlegend=True,
         title='Locations of ' + bookTitle,
 
-        margin=dict(
-            l=5,
-            r=5,
-            b=10,
-            t=70,
-            pad=2)
+        margin=dict(l=5, r=5, b=10, t=70, pad=2)
     )
+
+
     fig = Figure(data=data, layout=layout)
     # plots the graph
     plotly.offline.plot(fig, filename='map.html')
 
-# example
+
+# example for demo
 plotGraph('War and Peace', 'Leo Tolstoy', 'Russia', 'Signet', 'USA', '2012', '978-0451532114')
