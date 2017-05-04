e0889e2a0822b7be945e9f529e09f64cb53872a1
Sarah Barden <sarah.barden@students.olin.edu>
2017-05-03 20:09:01 -0400

    condensing three locations into a for loop

--- LocationsToMap.py
+++ LocationsToMap.py
@@ -1,5 +1,5 @@
 # Sarah Barden
-'''Takes an ISBN code and returns a plotly map highlighting the locations of
+'''Takes an ISBN code and returns a map highlighting the locations of
 the Author, Plot and Publisher.'''
 
 import folium   # library for data visualization on maps
@@ -36,23 +36,26 @@ def plotGraph(book):
     INPUT: a book object
     OUTPUT: a map with pins on locations
     """
-
+    # Creates the map
     map1 = folium.Map(zoom_start=2, tiles='Mapbox bright')
 
-    folium.Marker(geocode(book.authorLoc),
-                  icon=folium.Icon(color='red'),
-                  popup='{} is from {}'.format(book.author, book.authorLoc)
-                  ).add_to(map1)
+    # Adds marker for the each location
+    locations = [book.authorLoc, book.publisherLoc, book.plotLoc]
+    colors = ['red', 'green', 'blue']
+    popups = ['{} is from {}'.format(book.author, book.authorLoc),
+              'This edition was published in {}'.format(book.publisherLoc),
+              '{} takes place in {}'.format(book.title, book.plotLoc)]
+
+    for i in range(1, 3):
+        print(i)
+        if locations[i] == []:
+            pass
+        else:
+            folium.Marker(geocode(locations[i]),
+                          icon=folium.Icon(color=colors[i]),
+                          popup=popups[i]
+                          ).add_to(map1)
     sleep(1)
-    folium.Marker(geocode(book.publisherLoc),
-                  icon=folium.Icon(color='green'),
-                  popup='This edition was published in {}'.format(book.publisherLoc)
-                  ).add_to(map1)
-    sleep(1)
-    folium.Marker(geocode(book.plotLoc),
-                  icon=folium.Icon(color='blue'),
-                  popup='{} takes place in {}'.format(book.title, book.plotLoc),
-                  ).add_to(map1)
     map1.save('map.html')
 
 
